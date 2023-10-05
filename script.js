402751781bd355d49700270783b794217216a10e
Shahu Patil <139709118+Shahu2002@users.noreply.github.com>
2023-10-06 02:03:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your code here
+
+        // JavaScript for Bootstrap form validation
+        (function () {
+            'use strict';
+
+            window.addEventListener('load', function () {
+                var form = document.getElementById('internship-form');
+                form.addEventListener('submit', function (event) {
+                    if (!form.checkValidity()) {
+                        event.preventDefault();
+                        event.stopPropagation();
+                    }
+                    form.classList.add('was-validated');
+                }, false);
+            }, false);
+        })();
+  
